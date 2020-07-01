928e61d74afd19f06d38d13e30b295aa256c85a6
Kos-Mos <ChuyM203@gmail.com>
2020-07-01 01:54:18 -0400

    added password length function

--- Develop/script.js
+++ Develop/script.js
@@ -17,6 +17,25 @@ var passwordGeneration = function() {
   var passwordCharaSets = charaInclusions();
 };
 
+//This is the function that will prompt for **password length**
+var passwordLength = function() {
+  //First, lets ask the user for the length of the password using a prompt
+  var lengthPass = prompt("Enter Desired Length of New Password. Must Be Between 8 and 128 Characters Long.");
+
+  //Next, lets validate the user input(Whether user entered invalid length or entered nothing at all)
+  if (lengthPass < 8 || lengthPass > 128 || lengthPass === "") {
+    alert("Invalid Length Entry. Length Must Be Between 8 and 128 Characters Long.")
+    return passwordLength;
+  }
+
+  //This turns string into number. Note:refer to documentation if im still confused later. Thanks for showing me this
+  lengthPass = Math.floor(Number(lengthPass));
+
+  //This will send data from this function to layout function!
+  return lengthPass;
+}
+passwordLength();
+
 
 // Assignment Code Ending
 var generateBtn = document.querySelector("#generate");
