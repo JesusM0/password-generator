5ac9cf7f85c1389a5abb9724d741d1927ff3cd78
Kos-Mos <ChuyM203@gmail.com>
2020-07-01 01:38:24 -0400

    added first function/NickedNamed Layout or Blueprint Function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,24 @@
-// Assignment Code
+// Assignment Code Beginning
+// ****This time, lets start by making the function that will retrieve all the other functions' data*******
+// ****Note to Self: This could help plan out my next move??*****
+
+//Global Scope Password
+var password = "";
+
+//This is the function that will help plan out what we need, its a function but also a "blueprint"
+var passwordGeneration = function() {
+  // Call in Global Password here to send out at end
+  password = "";
+
+  //Create a function that will ask user for length of password. Note: Must be between 8 and 128 Chara long
+  var lengthPassword = passwordLength();
+
+  //Create a function that will determine all inclusions for Character Sets
+  var passwordCharaSets = charaInclusions();
+};
+
+
+// Assignment Code Ending
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
