0781910c2a650de4d5490007ef4fb715660415f7
Kos-Mos <ChuyM203@gmail.com>
2020-07-01 00:39:12 -0400

    added passLength function/check for valid length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
 // Assignment Code
+
+// This variable will store our password which will be a string
+var password = "";
+
+//Next, Create a series of functions for password length, char types included, and validation
+
+//First prompt user of length of password?
+var passLength = function() {
+  var passwordLength = prompt("Type the length of the desired password. Minimum limit is 8 character, maximum is 128.");
+
+  //Checks if user entered less than or more than minimum or maximum allowed
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Length Must Be Between 8 and 128 Characters Long!")
+    return passLength();
+  }
+}
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
